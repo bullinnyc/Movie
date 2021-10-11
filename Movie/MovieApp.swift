9bfb198e967b0297c0ae8744c83a5c576e020a10
bullinnyc <i8u11@icloud.com>
2021-10-11 13:52:52 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Dmitry Kononchuk on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
